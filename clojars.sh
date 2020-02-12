7a778fe3ad86047a4d2857497ec0cc0a27715de3
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2020-02-12 13:08:37 +0000

    update bump version script

---
+++ clojars.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+Echo "Checking environment variables CLOJARS_USERNAME and CLOJARS_PASSWORD."
+
+if [ -z $CLOJARS_USERNAME ]
+then
+    echo "CLOJARS_USERNAME env variable does not exists ! Exiting."
+    exit 1;
+fi
+
+if [ -z $CLOJARS_PASSWORD ]
+then
+    echo "CLOJARS_PASSWORD env variable does not exists ! Exiting."
+    exit 1;
+fi
+
+echo "Cleaning previous jar"
+rm clj-pgqueue.jar
+
+echo "Generating pom"
+clj -Spom
+
+echo "Generating JAR clj-pgqueue.jar"
+clj -A:pack  mach.pack.alpha.skinny --no-libs --project-path clj-pgqueue.jar
+
+echo "Deploying into Clojars"
+
+clojure -A:deploy
+
+echo "Done"
